0dc3256f95388b84429aef52fb1f6bbf88cd257e
Yunming Shao <shaoym87@gmail.com>
2017-03-23 22:41:34 -0400

    correct the check condition

--- src/tools.cpp
+++ src/tools.cpp
@@ -20,7 +20,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	// check the validity of the following inputs:
 	//  * the estimation vector size should not be zero
 	//  * the estimation vector size should equal ground truth vector size
-	if(estimations.size() != ground_truth.size() + 1
+	if(estimations.size() != ground_truth.size()
 			|| estimations.size() == 0){
 		cout << "Invalid estimation or ground_truth data" << endl;
 		return rmse;
