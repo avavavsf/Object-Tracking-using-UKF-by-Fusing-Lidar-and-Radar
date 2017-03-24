e015bd42cef6999725ac17b1361f826a046fa771
Yunming Shao <shaoym87@gmail.com>
2017-03-23 22:42:46 -0400

    add the build direcoty

---
+++ build/CMakeFiles/UnscentedKF.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UnscentedKF.dir/src/ukf.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/src/main.cpp.o"
+  "CMakeFiles/UnscentedKF.dir/src/tools.cpp.o"
+  "UnscentedKF.pdb"
+  "UnscentedKF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UnscentedKF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
